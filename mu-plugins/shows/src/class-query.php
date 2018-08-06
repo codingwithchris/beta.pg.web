5abbddf3207b7a4c6bb959c3226fe6c3fbd873e9
Christopher Hahn <chris@creativefuse.org>
2018-08-05 20:43:29 -0400

    Set Placeholders for Shows

---
+++ mu-plugins/shows/src/class-query.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Playground\Shows;
+
+
+class Query{
+
+    public get_next_show(){
+
+    }
+
+    public get_all_shows_this_season( $current_season ){
+
+    }
+
+    public get_show( $name ){
+
+    }
+
+    public get_remaining_shows_this_season(){
+
+    }
+
+
+}
---
+++ mu-plugins/shows/src/class-show.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Playground\Shows;
+
+
+class Show{
+
+    $settings = [];
+
+    public set_status(){
+
+    }
+
+    public set_primary_color(){
+
+    }
+
+    public set_secondary_color(){
+
+    }
+
+    public set_ticket_url(){
+
+    }
+
+    public set_date_range(){
+
+    }
+
+    public set_thumbnail(){
+
+    }
+
+    public get_ticket_url(){
+
+    }
+
+    public get_status(){
+
+    }
+
+    public get_color( $type ){
+
+    }
+
+    public get_date( $type ){
+
+    }
+
+    public get_author(){
+
+    }
+
+    public get_performance_location(){
+
+    }
+
+    public get_embed( $platform, $id ){
+
+
+    }
+
+    public get_synopsis(){
+
+
+    }
+
+    public get_artists( $type ){
+
+
+    }
+
+    public get_sponsors(){
+
+
+    }
+
+    public get_related_articles(){
+
+    }
+
+    public get_date_range(){
+
+
+    }
+
+    public get_thumbnail(){
+
+
+    }
+
+    public is_past(){
+
+    }
+
+    public is_rehearsing(){
+
+    }
+
+    public is_future(){
+
+    }
+
+
+}
